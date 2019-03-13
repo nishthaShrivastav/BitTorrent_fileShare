b4c8d2b29d8a24b85a04507abb2b491c1bf72a12
anagh <anagh@LAPTOP-J8K5JMJG>
2019-03-13 19:59:33 -0400

    getter setter for network information network.java

---
+++ BitTorrent-File-Sharing-Software/src/com/ufl/cise/cnt5106/network.java
@@ -0,0 +1,36 @@
+package com.ufl.cise.cnt5106;
+
+public class network{
+	
+	public String peerId;
+	public String hostName;
+	public int port_number;
+	//get set file sharing and hasfile variable to be done
+	
+	public String getPeerId() {
+		return peerId;
+	}
+
+	public void setPeerId(String peerId) {
+		this.peerId = peerId;
+	}
+
+	public String getHostName() {
+		return hostName;
+	}
+
+	public void setHostName(String hostName) {
+		this.hostName = hostName;
+	}
+
+	public int getPort() {
+		return port_number;
+	}
+
+	public void setPort(int port) {
+		this.port_number = port;
+	}
+	
+	
+
+}
